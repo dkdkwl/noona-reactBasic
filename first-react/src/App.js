41ea91cfba6b9be7c050998dd607be5e448f7ede
dkdkwl <dkdkwl@naver.com>
2022-11-03 15:29:55 +0900

    state활용하기

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Box from "./componet/Box";
+import { useState } from 'react';
 
 function App() {
+  let counter = 0;
+  const [counter2,setCounter2] = useState(0);
+  const increase = () => {
+    counter = counter + 1;
+    setCounter2(counter2 + 1);
+  };
   return (
     <div>
-      <Box name = "리사" num = {1} />
-      <Box name = "제니" num = {2} />
-      <Box name = "지수" num = {3} />
+      <div>{counter}</div>
+      <div>state:{counter2}</div>
+      <button onClick={increase}>클릭</button>
     </div>
   );
 }
